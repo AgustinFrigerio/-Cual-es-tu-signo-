62835585bbc617c42febc9d5aa3aa6d06de95964
agustin <agustinfrigerio10@gmail.com>
2021-02-23 11:02:37 -0300

    cambios para mejorar el input

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//archivo para funcionalidad de la app
+<script>
+    $('input').on('focusin', function(){
+        if (this).parent().find('label').addClass('active');
+    });
+    $('input').on('focusout', function(){
+        if (!this.value){
+            $(this).parent().find('label').removeClass('active');
+        }
+
+    }); 
+</script>//archivo para funcionalidad de la app 
